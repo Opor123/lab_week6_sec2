4bf39fd8ffb6e079f8e18dac7bbfbf38c229b770
Opor123 <you@example.com>
2024-08-08 09:02:22 +0700

    1

---
+++ lab6/assignment2.py
@@ -0,0 +1,12 @@
+"""
+### Assignment 2: Basic Exception Handling
+**Required Knowledge**: Basic Exception Handling, Try-Except Block
+**Instructions**:
+1. Modify the `Car` class to include a method named `print_car_info()` that prints the car's make and model.
+2. Add exception handling in the `print_car_info()` method using a try-except block to handle potential errors (e.g., if the attributes are not set using AttributeError to handle exception).
+3. Test the method by:
+   
+Creating a `car1` object with valid `make` and `model` attributes and calling `print_car_info()`.
+   
+Creating a `car2` object without setting the attributes (or by setting them to `None`) and calling `print_car_info()` to see how the exception is handled.
+"""
